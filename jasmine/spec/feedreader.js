881d9adffb4f809207eecfc29d55fa8950ef1421
Matilde Wittrup <matildeene@outlook.com>
2018-01-08 17:11:30 +0100

    Testing if the menubar is clickable

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -55,20 +55,27 @@ $(function () {
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function () {
 
-    /* TODO: Write a test that ensures the menu element is
-     * hidden by default. You'll have to analyze the HTML and
-     * the CSS to determine how we're performing the
-     * hiding/showing of the menu element.
-     */
-    it('Menubar is hidden', function () {
-        expect($('body').hasClass('menu-hidden')).toBe(true);
-    });
+        /* TODO: Write a test that ensures the menu element is
+         * hidden by default. You'll have to analyze the HTML and
+         * the CSS to determine how we're performing the
+         * hiding/showing of the menu element.
+         */
+        it('Menubar is hidden', function () {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
 
-    /* TODO: Write a test that ensures the menu changes
-     * visibility when the menu icon is clicked. This test
-     * should have two expectations: does the menu display when
-     * clicked and does it hide when clicked again.
-     */
+        /* TODO: Write a test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
+        it('Menubar displays if the user click the icon', function () {
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
@@ -78,7 +85,6 @@ $(function () {
      * Remember, loadFeed() is asynchronous so this test will require
      * the use of Jasmine's beforeEach and asynchronous done() function.
      */
-    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
